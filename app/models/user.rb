a1791d8f46c630fb9aba9fd72d205ee2b1070129
caelum-2568 <152895310+caelum-2568@users.noreply.github.com>
2024-01-12 18:13:30 +0900

    ユーザー管理機能_テストコード完了_3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/.freeze, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/.freeze, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
-  validates :sending_kana_for_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
-  validates :sending_kana_for_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :sending_kana_for_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角カタカナで入力してください" }
+  validates :sending_kana_for_last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角カタカナで入力してください" }
   validates :date_of_birth, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数字混合で入力してください" }
 
