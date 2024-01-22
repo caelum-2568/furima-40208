ba2d8eefc06b3ef613d2f1c023555f419e5d09e5
caelum-2568 <152895310+caelum-2568@users.noreply.github.com>
2024-01-22 14:36:15 +0900

    商品削除機能_実装_002

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,6 @@ class ItemsController < ApplicationController
   
 
     def destroy
-      @item = Item.find(params[:id])
       if @item.user_id == current_user.id
         @item.destroy
         redirect_to root_path, notice: '商品が削除されました。'
