9a661695a9c6dec354c4c210726a983dccc33021
caelum-2568 <152895310+caelum-2568@users.noreply.github.com>
2024-01-22 14:25:30 +0900

    商品削除機能_実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -33,6 +33,18 @@ class ItemsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+  
+
+    def destroy
+      @item = Item.find(params[:id])
+      if @item.user_id == current_user.id
+        @item.destroy
+        redirect_to root_path, notice: '商品が削除されました。'
+      else
+        redirect_to item_path(@item), alert: '削除権限がありません。'
+      end
+    end
+
 
   private
 
