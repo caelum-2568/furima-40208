65e56e982299cef1de291e86289b73f3df8a099a
caelum-2568 <152895310+caelum-2568@users.noreply.github.com>
2024-01-25 15:46:58 +0900

    デプロイ後の修正_002

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,31 +1,36 @@
-const pay = () => {
-  const publicKey = gon.public_key
-  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
-  const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
+let isPayjpInitialized = false;
 
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
-  const form = document.getElementById('charge-form')
-  form.addEventListener("submit", (e) => {
-    payjp.createToken(numberElement).then(function (response) {
-      if (response.error) {
-      } else {
-        const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      }
-      numberElement.clear();
-      expiryElement.clear();
-      cvcElement.clear();
-      document.getElementById("charge-form").submit();
-    });
-    e.preventDefault();
-  });
+const pay = () => {
+  if (!isPayjpInitialized) {
+    const publicKey = gon.public_key;
+    const payjp = Payjp(publicKey); // PAY.JPテスト公開鍵
+    const elements = payjp.elements();
+    const numberElement = elements.create('cardNumber');
+    const expiryElement = elements.create('cardExpiry');
+    const cvcElement = elements.create('cardCvc');
+
+    numberElement.mount('#number-form');
+    expiryElement.mount('#expiry-form');
+    cvcElement.mount('#cvc-form');
+
+    const form = document.getElementById('charge-form');
+    if (form) {
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        payjp.createToken(numberElement).then(function (response) {
+          if (!response.error) {
+            const token = response.id;
+            const renderDom = document.getElementById("charge-form");
+            const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+            renderDom.insertAdjacentHTML("beforeend", tokenObj);
+            form.submit();
+          }
+        });
+      });
+    }
+
+    isPayjpInitialized = true;
+  }
 };
 
 window.addEventListener("turbo:load", pay);
